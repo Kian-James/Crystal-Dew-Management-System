68d181c0e98d7d6b65ef38011dfb8962b007f9f1
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-02-20 21:18:08 +0800

    Dash Changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,7 +5,24 @@ const Dashboard = () => {
   return (
     <Layout title={"Dashboard"}>
       <div className="dashboard-main-container">
-        <div className="dashboard-container"></div>
+        <div className="dashboard-container">
+          <div className="nav-container"></div>
+          <div className="main-container">
+            <div className="top-bar">
+              <div className="logo-container">
+                <img
+                  alt="school-logo"
+                  className="school-logo"
+                  src={"../components/Images/PBSI-LOGO.png"}
+                />
+              </div>
+              <div className="profile-container">
+                <h2 className="profile-name">PBSI</h2>
+                <button className="logout-btn">Logout</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
