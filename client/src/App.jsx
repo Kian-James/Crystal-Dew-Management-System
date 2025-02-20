a624ace9ca7a58160f4637b203456e4e957db582
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-02-20 20:19:32 +0800

    Register or Enrollment Page To be Determined in the Future

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
