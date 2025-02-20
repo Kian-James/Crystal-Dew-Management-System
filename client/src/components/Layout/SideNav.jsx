201cf418c2a009f0b11a38fdad11041ae595afa5
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-02-20 21:55:18 +0800

    Menu-Link

--- client/src/components/Layout/SideNav.jsx
+++ client/src/components/Layout/SideNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SideNav = () => {
   return (
@@ -13,6 +14,15 @@ const SideNav = () => {
           <p className="brand-name">PBSI </p>
           <p className="brand-slogan">Manage your School in Easy Way</p>
         </div>
+      </div>
+      <div className="menu-container">
+        <Link className="menu-link">Home</Link>
+        <Link className="menu-link">All Course</Link>
+        <Link className="menu-link">Add Course</Link>
+        <Link className="menu-link">Add Students</Link>
+        <Link className="menu-link">All Students</Link>
+        <Link className="menu-link">Collect Fee</Link>
+        <Link className="menu-link">Payment History</Link>
       </div>
     </div>
   );
