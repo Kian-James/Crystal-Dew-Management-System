6df9b9b84648877a59d0420a1e282b9fa555d734
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-02-25 08:46:15 +0800

    Formatting in Date

--- server/models/tModel.js
+++ server/models/tModel.js
@@ -32,7 +32,13 @@ const transactionSchema = new mongoose.Schema({
   },
   transaction_date: {
     type: String,
-    default: () => new Date().toISOString().split("T")[0],
+    default: () => {
+      const date = new Date();
+      const mm = String(date.getMonth() + 1).padStart(2, "0");
+      const dd = String(date.getDate()).padStart(2, "0");
+      const yyyy = date.getFullYear();
+      return `${mm}/${dd}/${yyyy}`;
+    },
   },
 });
 
