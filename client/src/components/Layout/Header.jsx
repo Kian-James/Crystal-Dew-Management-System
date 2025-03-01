34a4f861fce19565d6e979269d2e5f4133a653f4
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-03-01 14:21:09 +0800

    Reapply "temp home & header"
    This reverts commit 5d1642313c2191e08219f2532222ca68d3fc08f4.

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -2,6 +2,7 @@ import { NavLink, Link } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import Dashboard from "../../pages/User/Dashboard";
+import "./Header.css"; // Import the custom CSS file
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
@@ -16,7 +17,7 @@ const Header = () => {
   };
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
@@ -31,23 +32,23 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <img
-              src={"../components/Images/PBSI-LOGO-1.png"}
+              src={"/src/components/Images/CD-Logo.jpg"}
               alt="brand-logo"
               className="brand-logo"
             />
-            <Link to="/" className="navbar-brand">
+            <Link to="/" className="navbar-brand custom-brand">
               Crystal Dew Water Station
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink to="/" className="nav-link">
+                <NavLink to="/" className="nav-link custom-nav-link">
                   Home
                 </NavLink>
               </li>
               {!auth.user ? (
                 <>
                   <li className="nav-item">
-                    <NavLink to="/login" className="nav-link">
+                    <NavLink to="/login" className="nav-link custom-nav-link">
                       Login
                     </NavLink>
                   </li>
@@ -61,17 +62,19 @@ const Header = () => {
                           ? "/dashboard"
                           : "/employee-dashboard"
                       }
-                      className="nav-link"
+                      className="nav-link custom-nav-link"
                     >
                       Dashboard
                     </NavLink>
                   </li>
-                  <span className="navbar-text">{auth.user.name}!</span>
+                  <span className="navbar-text custom-navbar-text">
+                    {auth.user.name}!
+                  </span>
                   <li className="nav-item">
                     <NavLink
                       onClick={handleLogout}
                       to="/login"
-                      className="nav-link"
+                      className="nav-link custom-nav-link"
                     >
                       Logout
                     </NavLink>
