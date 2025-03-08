8955bccd4bb7788c83b52179a49a2af19fd47e52
margoxt <angelicatinio14@gmail.com>
2025-03-08 17:36:22 +0800

    Style: Adjusted login.jsx, login.css, header for responsiveness. Added home design and about us page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,44 +7,52 @@ import "./Home.css";
 const Home = () => {
   return (
     <Layout title={"Home"}>
-    <div className="vid-container">
-      <video className="bg-video" src={vd1} autoPlay muted loop>
-      </video>
-      <div className="vid-overlay position-absolute top-50 end-0 translate-middle-y d-flex flex-column p-0 m-0">
-        <div className="content">
-          <section className="home text-white d-flex align-items-center min-vh-100">
-            <div className="container text-center">
-              <div className="row justify-content-center">
-                <div className="col-md-8">
-                  <h2 className="display-4 fw-bold text-uppercase mt-4">
-                    <em>Pure, Fresh, Crystal Clear Hydration You Can Trust!</em>
-                  </h2>
-                  <p className="lead">At Crystal Dew, we are dedicated to providing the highest quality 
-                    drinking water for you and your family. Our advanced purification process ensures 
-                    every drop is fresh, clean, and safe. Whether you need refills, doorstep delivery, 
-                    or a convenient walk-in station, we are committed to keeping you hydrated with pure 
-                    and refreshing water. Experience the difference with Crystal Dew—where purity meets 
-                    trust.</p>
+      <div className="vid-container">
+        <video className="bg-video" src={vd1} autoPlay muted loop></video>
+        <div className="vid-overlay position-absolute top-50 end-0 translate-middle-y d-flex flex-column p-0 m-0">
+          <div className="content">
+            <section className="home text-white d-flex align-items-center min-vh-100">
+              <div className="container text-center">
+                <div className="row justify-content-center">
+                  <div className="col-md-8">
+                    <h2 className="display-4 fw-bold text-uppercase mt-4">
+                      <em>
+                        Pure, Fresh, Crystal Clear Hydration You Can Trust!
+                      </em>
+                    </h2>
+                    <p className="lead">
+                      At Crystal Dew, we are dedicated to providing the highest
+                      quality drinking water for you and your family. Our
+                      advanced purification process ensures every drop is fresh,
+                      clean, and safe. Whether you need refills, doorstep
+                      delivery, or a convenient walk-in station, we are
+                      committed to keeping you hydrated with pure and refreshing
+                      water. Experience the difference with Crystal Dew—where
+                      purity meets trust.
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            {/* Social Media Icons */}
-            <div className="social-media position-absolute top-50 end-0 translate-middle-y me-3 d-flex flex-column">
-              <a href="https://www.facebook.com/crystal.dew.pandan" className="mb-3 text-white fs-3">
-                <FaFacebook />
-              </a>
-              <a href="https:/m.me/crystal.dew.pandan" className="text-white fs-3">
-                <FaFacebookMessenger />
-              </a>
-            </div>
-          </section>
-
-
+              {/* Social Media Icons */}
+              <div className="social-media position-absolute top-50 end-0 translate-middle-y me-3 d-flex flex-column">
+                <a
+                  href="https://www.facebook.com/crystal.dew.pandan"
+                  className="mb-3 text-white fs-3"
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https:/m.me/crystal.dew.pandan"
+                  className="text-white fs-3"
+                >
+                  <FaFacebookMessenger />
+                </a>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
-
     </Layout>
   );
 };
