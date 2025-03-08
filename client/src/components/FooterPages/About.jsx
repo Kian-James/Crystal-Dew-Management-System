2ea33b235c7084e5d28dfd13aef8a7b1b696b778
margoxt <angelicatinio14@gmail.com>
2025-03-08 20:57:35 +0800

    Style: Finalized the background of About and Home page

--- client/src/components/FooterPages/About.jsx
+++ client/src/components/FooterPages/About.jsx
@@ -3,58 +3,64 @@ import { FaBuildingUser } from "react-icons/fa6";
 import { IoWater } from "react-icons/io5";
 import { TbTruckDelivery } from "react-icons/tb";
 import Layout from "../Layout/Layout";
+import vd2 from "../Videos/backgroundVid2.mp4";
 import "./About.css";
 
 const About = () => {
   return (
-    <div>
       <Layout title={"About Us"}>
       <div className="about-page">
-          <div className="about-overlay"></div>
-          <div className="row contact ">
-              <div className="col-md-6 ">
-              <div className="card shadow-lg">
-                <h2 className = "title fw-bold" >Our Service Section</h2>
-                <ul className="service-list">
-                  <li>
-                    <IoWater className="icon" />
-                    <p className="service-content d-inline">
-                      <em><strong>Refilling Services: </strong></em>
-                      Bring your own container, or use ours.
-                    </p>
-                  </li>
-                  <li>
-                    <TbTruckDelivery className="icon" />
-                    <p className="service-content d-inline">
-                      <em><strong>Delivery Services: </strong></em>
-                      Fast and convenient water delivery to your doorstep.
-                    </p>
-                  </li>
-                  <li>
-                    <FaBuildingUser className="icon" />
-                    <p className="service-content d-inline">
-                      <em><strong>Walk-in Station: </strong></em>
-                      Visit us anytime for fresh and clean water.
-                    </p>
-                  </li>
-                </ul>
+        <div className="vid-container">
+              <video className="bg-video" src={vd2} autoPlay muted loop>
+              </video>
+            <div className="vid-overlay position-absolute top-50 end-0 translate-middle-y d-flex flex-column p-0 m-0">
+            <div className="content"></div>
+            <div className="container d-flex justify-content-center align-items-center min-vh-100">
+                <div className="col-md-6 mx-2">
+                <div className="card shadow-lg">
+                  <h2 className = "title fw-bold" >Our Service Section</h2>
+                  <ul className="service-list">
+                    <li>
+                      <IoWater className="icon" />
+                      <p className="service-content d-inline">
+                        <em><strong>Refilling Services: </strong></em>
+                        Bring your own container, or use ours.
+                      </p>
+                    </li>
+                    <li>
+                      <TbTruckDelivery className="icon" />
+                      <p className="service-content d-inline">
+                        <em><strong>Delivery Services: </strong></em>
+                        Fast and convenient water delivery to your doorstep.
+                      </p>
+                    </li>
+                    <li>
+                      <FaBuildingUser className="icon" />
+                      <p className="service-content d-inline">
+                        <em><strong>Walk-in Station: </strong></em>
+                        Visit us anytime for fresh and clean water.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-            <div className="col-md-4">
-            <div className="card shadow-lg" style={{ width: "100%", height: "335px" }}>
-            <h2 className="title fw-bold">About Us</h2>
-              <p className="text-justify mt-2">
-                  At Crystal Dew, we prioritize quality and purity. Our advanced filtration 
-                  system ensures every drop meets the highest standards of safety and taste. 
-                  From homes to businesses, we are committed to keeping our community hydrated 
-                  with clean and refreshing drinking water.
-              </p>
-            </div>
+              <div className="col-md-4 mx-2">
+              <div className="card shadow-lg" style={{ width: "100%", height: "370px" }}>
+              <h2 className="title fw-bold">About Us</h2>
+                <p className="text-justify mt-2">
+                    We opened during the pandemic and we started providing clean and safe
+                    water to our community. Slowly, we were able to help small businesses
+                    have their quality  water as well. We gained our customer&apos;s 
+                    trust and loyalty. We are grateful for everyone who has supported us, 
+                    we&apos;ll continue to provide the best quality water.
+                </p>
+              </div>
+              </div>
             </div>
           </div>
         </div>
+        </div>
       </Layout>
-    </div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,50 @@
 import React from "react";
 import { FaFacebook, FaFacebookMessenger } from "react-icons/fa";
 import Layout from "../components/Layout/Layout";
+import vd1 from "../components/Videos/backgroundVid.mp4";
 import "./Home.css";
-
-// import vd1 from "../../components/Videos/1-w-bg.mp4"
 
 const Home = () => {
   return (
     <Layout title={"Home"}>
-{/* 
-    Let's see lang what if
-    <video className="bg-video" src={vd1} autoPlay muted loop>
-      
-    </video> */}
-
-      <section className="home text-white d-flex align-items-center min-vh-100"style={{ backgroundColor: "var(--darkest-color)" }}>
-        <div className="container text-center">
-          <div className="row justify-content-center">
-            <div className="col-md-8">
-              <h2 className="display-4 fw-bold text-uppercase mt-4">
-                <em>Pure, Fresh, Crystal Clear Hydration You Can Trust!</em>
-              </h2>
-              <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                laborum.</p>
+    <div className="vid-container">
+      <video className="bg-video" src={vd1} autoPlay muted loop>
+      </video>
+      <div className="vid-overlay position-absolute top-50 end-0 translate-middle-y d-flex flex-column p-0 m-0">
+        <div className="content">
+          <section className="home text-white d-flex align-items-center min-vh-100">
+            <div className="container text-center">
+              <div className="row justify-content-center">
+                <div className="col-md-8">
+                  <h2 className="display-4 fw-bold text-uppercase mt-4">
+                    <em>Pure, Fresh, Crystal Clear Hydration You Can Trust!</em>
+                  </h2>
+                  <p className="lead">At Crystal Dew, we are dedicated to providing the highest quality 
+                    drinking water for you and your family. Our advanced purification process ensures 
+                    every drop is fresh, clean, and safe. Whether you need refills, doorstep delivery, 
+                    or a convenient walk-in station, we are committed to keeping you hydrated with pure 
+                    and refreshing water. Experience the difference with Crystal Dew—where purity meets 
+                    trust.</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        {/* Social Media Icons */}
-        <div className="position-absolute top-50 end-0 translate-middle-y me-3 d-flex flex-column">
-          <a href="https://www.facebook.com/crystal.dew.pandan" className="mb-3 text-white fs-3">
-            <FaFacebook />
-          </a>
-          <a href="https:/m.me/crystal.dew.pandan" className="text-white fs-3">
-            <FaFacebookMessenger />
-          </a>
+            {/* Social Media Icons */}
+            <div className="social-media position-absolute top-50 end-0 translate-middle-y me-3 d-flex flex-column">
+              <a href="https://www.facebook.com/crystal.dew.pandan" className="mb-3 text-white fs-3">
+                <FaFacebook />
+              </a>
+              <a href="https:/m.me/crystal.dew.pandan" className="text-white fs-3">
+                <FaFacebookMessenger />
+              </a>
+            </div>
+          </section>
+
+
         </div>
-      </section>
+      </div>
+    </div>
+
     </Layout>
   );
 };
