fc70e90266244b339ee6920fc4635e56bd58cd1b
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-03-09 09:08:09 +0800

    Deployement Changes

--- server/server.js
+++ server/server.js
@@ -22,7 +22,13 @@ app.get("/", (req, res) => {
 connectDB();
 
 // MIDDLEWARES
-app.use(cors());
+app.use(
+  cors({
+    origin: "crystal-dew-management-system-frontend.vercel.app",
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
