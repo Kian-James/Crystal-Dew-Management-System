977d1f0997b7c057ec1aaa840ca7559140f7911d
Kian James <148171376+Siesta0922@users.noreply.github.com>
2025-03-09 11:54:47 +0800

    Imagemin Fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { imagemin } from "vite-plugin-imagemin";
+import imagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
