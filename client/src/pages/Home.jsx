844d48ebd92c411b6467a04a1eb3158d0a72c476
Jero Halili <jerohalili07@gmail.com>
2025-03-09 21:45:12 +0800

    fix home responsiveness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
               <div className="container text-center home-content">
                 <div className="row justify-content-center">
                   <div className="col-md-8">
-                    <h2 className="display-4 fw-bold text-uppercase mt-4">
+                    <h2 className="display-4 fw-bold text-uppercase">
                       <em>
                         Pure, Fresh, Crystal Clear Hydration You Can Trust!
                       </em>
